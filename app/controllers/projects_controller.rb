6f45b27f3cc25286a747c43f5e1b1e2863defbd5
audrie <audrie.talbot@gmail.com>
2015-07-17 16:58:38 -0700

    Adds description field.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -24,6 +24,6 @@ class ProjectsController < ApplicationController
     end
     private
     def project_params
-      params.require(:project).permit(:name)
+      params.require(:project).permit(:name, :description)
   end
 end
