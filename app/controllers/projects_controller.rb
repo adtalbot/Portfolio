1bc3177c28119ab59240963fa9413fac7909c284
audrie <audrie.talbot@gmail.com>
2015-07-17 11:03:59 -0700

    New route functioning.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,4 +8,22 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
     render :show
   end
+
+  def new
+    @project = Project.new
+    render :new
+  end
+
+  def create
+    @project = Project.new(project_params)
+      if @project.save
+        redirect_to projects_path
+      else
+        render :new
+      end
+    end
+    private
+    def project_params
+      params.require(:project).permit(:name)
+  end
 end
